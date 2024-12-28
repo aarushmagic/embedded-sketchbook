d434e1689a4bbbcae3856aa3f75743eb90e2abb7
aarushmagic <height@aarushmagic.com>
2024-12-28 11:56:00 -0500

    Added bluetooth capabilities

---
+++ Bluetooth Speaker/README.md
@@ -0,0 +1,7 @@
+# ESP32 Bluetooth Speaker
+A custom-built speaker driver made from a paper bowl and copper coil, driven by an ESP32 via Bluetooth A2DP.
+
+## Current Status
+Bluetooth works, but driving the coil directly from the ESP32 GPIO results in very low volume.
+
+![Direct Drive Circuit](./assets/circuit_v1_direct.jpg)
--- Bluetooth Speaker/src/main.ino
+++ Bluetooth Speaker/src/main.ino
@@ -1,53 +1,15 @@
-#include <Arduino.h>
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
 
-#define SPEAKER_PIN 25
+AnalogAudioStream out;
+BluetoothA2DPSink a2dp_sink(out);
 
 void setup() {
-
-}
-
-void playKick() {
-  float frequency = 150.0; 
-  float volume = 255.0;
-
-  while (frequency > 40.0) {
-    float period = 1000000.0 / frequency;
-
-    for (int t = 0; t < period; t += 20) {
-      float angle = (float)t / period * 2.0 * PI;
-      int value = (sin(angle) + 1.0) * (volume / 2.0);
-
-      dacWrite(SPEAKER_PIN, value);
-      delayMicroseconds(10);
-    }
-
-    frequency -= 3.0; 
-    volume -= 4.0; 
-  }
-  dacWrite(SPEAKER_PIN, 0);
-}
-
-void playSnare() {
-  int volume = 200;
-
-  for (int i = 0; i < 800; i++) {
-    int randomValue = random(0, volume);
-    dacWrite(SPEAKER_PIN, randomValue);
-    delayMicroseconds(40);
-    if (i % 5 == 0 && volume > 0) volume--; 
-  }
-  dacWrite(SPEAKER_PIN, 0);
+  Serial.begin(115200);
+  out.begin();
+  a2dp_sink.start("PaperBowlSpeaker");
 }
 
 void loop() {
-  playKick();
-  delay(200);
-  playSnare();
-  delay(200);
-  playKick();
-  delay(100);
-  playKick();
-  delay(100);
-  playSnare();
-  delay(400);
+    
 }
